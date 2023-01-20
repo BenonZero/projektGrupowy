92b058acfd8cb67e0fac0fa43f0980386deb2e32
ElCresp0 <jacky8203@protonmail.com>
2023-01-20 21:54:01 +0100

    początki kodowania

---
+++ E2E_KS_Attention_Energy_Scorer/e2e_model.py
@@ -0,0 +1,82 @@
+from python_speech_features import mfcc
+from python_speech_features import logfbank
+from torch import tensor, nn, ones, functional as F
+# import numpy
+
+class SpeechEncDec(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        self.add_module("first_cnn", nn.Conv1d(13, 128, 1))
+        # "Number of filters = Number of out_channels."
+        # "Kernel size of 3 works fine everywhere"
+        self.add_module("second_cnn", nn.Conv1d(128, 256, 1))
+        self.add_module("pool", nn.MaxPool1d(3, 2))
+        self.add_module("gruencoder", nn.GRU(input_size = 256, hidden_size = 256, num_layers = 256))
+        # self.add_module() RELU
+        self.add_module("grudecoder", nn.GRU(input_size = 256, hidden_size = 256, num_layers = 256))
+        # batched input -> output(L,N,H)
+
+    def forward(self, X):
+        modules = self.named_modules()
+        d = dict(modules)
+        # print(d)
+        # for idx, module in enumerate(modules):
+        #     print(module[0])
+        # print(modules)
+        # Y = []
+        # for x in X:
+        #     tmp = [[x]]
+        #     Y.append((d["first_cnn"]).forward(tensor(tmp)))
+
+        # print(d["first_cnn"])
+        # print(tensor(X))
+        # print(ones(1, 13, 1))
+        # print(type(ones(1)[0]))
+        # print(tensor([X[0]]))
+        # print((d["first_cnn"]).forward(ones(1, 13, 1))) #.forward(tensor([X])))
+        # print((d["first_cnn"]).forward(tensor([X[0]])))
+        print(len(X))
+        print(len(X[0]))
+        print(len(X[0][0]))
+        Y = [(d["first_cnn"]).forward(tensor([x])) for x in X]
+        # print(Y)
+        tmp = X
+        X = Y
+        print(len(X))
+        print(len(X[0]))
+        print(len(X[0][0]))
+        Y = tmp
+        # print(X)
+        X = [[[X[0][0][i]] for i in range(len(X[0][0]))] for j in range(len(X))]
+        print(len(X))
+        print(len(X[0]))
+        print(len(X[0][0]))
+        Y = [(d["second_cnn"]).forward(tensor([x])) for x in X]
+        tmp = X
+        X = Y
+        print(len(X))
+        print(len(X[0]))
+        print(len(X[0][0]))
+        Y = tmp
+        # print(X)
+        X = [[[X[0][0][i]] for i in range(len(X[0][0]))] for j in range(len(X))]
+        print(len(X))
+        print(len(X[0]))
+        print(len(X[0][0]))
+        # print(len(Y))
+        # print(len(Y[0]))
+        # print(len(Y[0][0]))
+        # print(Y)
+        # print(X)
+
+
+sed = SpeechEncDec()
+sed.forward([[[float(i+j)] for i in range(13)] for j in range(5)])
+
+
+# from python_speech_features import mfcc
+# from python_speech_features import logfbank
+# import scipy.io.wavfile as wav
+# (rate,sig) = wav.read(".wav")
+# mfcc_feat = mfcc(sig,rate) # -> array[n][13]
+# fbank_feat = logfbank(sig, rate)
