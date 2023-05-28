fb72d790499813a8b753acc6f566bc548dc1f96c
Jakub Kwiatkowski <123kuba20010802@gmail.com>
2023-05-29 01:25:00 +0200

    Add files via upload

--- S1DCNN/modelik.py
+++ S1DCNN/modelik.py
@@ -5,39 +5,22 @@ import torch.nn.functional as F
 class Stacked1DCNN(nn.Module):
     def __init__(self,number_of_vectors_layer_1,number_of_vectors_layer_2,number_of_filters,kernel_size):
         super().__init__()
-        self.layer1=nn.Conv1d(in_channels=number_of_vectors_layer_1, out_channels=number_of_filters, kernel_size=kernel_size)     #layer1
-        self.relu=nn.ReLU()
-        self.layer2=nn.Conv1d(in_channels=number_of_vectors_layer_2, out_channels=number_of_filters, kernel_size=kernel_size)     #layer2
+        self.model = nn.Sequential(
+            nn.Conv1d(in_channels=number_of_vectors_layer_1, out_channels=number_of_filters, kernel_size=kernel_size),
+            # layer1
+            nn.ReLU(),
+            nn.Conv1d(in_channels=number_of_vectors_layer_2, out_channels=number_of_filters, kernel_size=kernel_size)
+            # layer2
+        )
         self.fc=nn.LazyLinear(1)
         self.softmax=nn.Softmax(dim=0)
         self.flatten=nn.Flatten(start_dim=1)
     def forward(self, x):
-        #networks = []
-        #for i in range(7):
-            #networks.append(self.model(x))
-        x1=self.layer1(x)
-        x2=self.layer1(x)
-        x3=self.layer1(x)
-        x4=self.layer1(x)
-        x5=self.layer1(x)
-        x6=self.layer1(x)
-        x7=self.layer1(x)
-        x1=self.relu(x1)
-        x2=self.relu(x2)
-        x3=self.relu(x3)
-        x4=self.relu(x4)
-        x5=self.relu(x5)
-        x6=self.relu(x6)
-        x7=self.relu(x7)
-        x1 = self.layer2(x1)
-        x2 = self.layer2(x2)
-        x3 = self.layer2(x3)
-        x4 = self.layer2(x4)
-        x5 = self.layer2(x5)
-        x6 = self.layer2(x6)
-        x7 = self.layer2(x7)
-        stacked=torch.stack((x1,x2,x3,x4,x5,x6,x7),1)
+        networks = []
+        for i in range(7):
+            networks.append(self.model(x))
+        stacked=torch.stack(networks,1)
         stacked=self.flatten(stacked)
         stacked=self.fc(stacked)
-        return F.log_softmax(x1, dim=0)
+        return F.log_softmax(stacked, dim=0)
 
